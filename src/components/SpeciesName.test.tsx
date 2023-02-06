18920feb3be6d6fc70285a92d2ac7d5eb78ad1c0
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-02-06 19:32:56 +0200

    add test that input has value from SpeciesName component props

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,4 +1,3 @@
-//Homo sapiens
 import { render, screen } from "@testing-library/react";
 import SpeciesName from "./SpeciesName";
 
@@ -11,5 +10,15 @@ describe("SpeciesName component", () => {
     render(<SpeciesName {...props} />);
     const labelText = screen.getByText("Species Name");
     expect(labelText).toBeInTheDocument();
+  });
+
+  it("GIVEN the component props THEN when component is rendered THEN value from props should be displayed", () => {
+    const props = {
+      speciesName: "Homo sapiens",
+      onChangeSpeciesName: jest.fn(),
+    };
+    render(<SpeciesName {...props} />);
+    const input = screen.getByRole("textbox");
+    expect(input).toHaveValue("Homo sapiens");
   });
 });
