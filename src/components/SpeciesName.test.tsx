6a6498470da907f96da3d756d742b3610888953d
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-02-08 18:51:34 +0200

    add test that value from props should be displayed

--- src/components/SpeciesName.test.tsx
+++ src/components/SpeciesName.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import SpeciesName from "./SpeciesName";
 
 describe("SpeciesName component", () => {
-  it("GIVEN the component props THEN when component is rendered THEN label text should be rendered", () => {
+  it("GIVEN the component props WHEN when component is rendered THEN label text should be rendered", () => {
     const props = {
       speciesName: "",
       onChangeSpeciesName: jest.fn(),
@@ -12,7 +14,7 @@ describe("SpeciesName component", () => {
     expect(labelText).toBeInTheDocument();
   });
 
-  it("GIVEN the component props THEN when component is rendered THEN value from props should be displayed", () => {
+  it("GIVEN the component props WHEN when component is rendered THEN value from props should be displayed", () => {
     const props = {
       speciesName: "Homo sapiens",
       onChangeSpeciesName: jest.fn(),
@@ -20,5 +22,20 @@ describe("SpeciesName component", () => {
     render(<SpeciesName {...props} />);
     const input = screen.getByRole("textbox");
     expect(input).toHaveValue("Homo sapiens");
+  });
+
+  it("GIVEN the component props WHEN when onChange is called THEN value from props should be displayed", async () => {
+    const mockOnChange = jest.fn();
+    const props = {
+      speciesName: "Homo sapiens",
+      onChangeSpeciesName: mockOnChange,
+    };
+    render(<SpeciesName {...props} />);
+
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "Homo sapiens");
+    expect(input).toHaveValue("Homo sapiens");
+    expect(mockOnChange).toHaveBeenCalled();
+    expect(mockOnChange).toHaveBeenCalledWith("Homo sapiens");
   });
 });
