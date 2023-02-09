dad21098371d5010ee770d1f64812b1cd8667f41
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-02-09 05:34:40 +0200

    add WhatIs2plus2 validation and tests

--- src/components/WhatIs2plus2.test.tsx
+++ src/components/WhatIs2plus2.test.tsx
@@ -15,29 +15,46 @@ describe("WhatIs2plus2", () => {
     expect(screen.getByText("What is 2 + 2?")).toBeInTheDocument();
   });
 
-  it("GIVEN the componet props WHEN component is rendered THEN the value from props should be displayed", () => {
-    const props = {
-      whatIs2plus2: "Not 4" as Answer,
-      setWhatIs2plus2: jest.fn(),
-    };
-    render(<WhatIs2plus2 {...props} />);
-    expect(screen.getByRole("combobox")).toHaveValue("Not 4");
-  });
-
   it("GIVEN the componet props WHEN component onChange is called THEN the value from props should be displayed", async () => {
+    const mockFunc = jest.fn();
     const props = {
       whatIs2plus2: "4" as Answer,
-      setWhatIs2plus2: jest.fn(),
+      setWhatIs2plus2: mockFunc,
     };
     render(<WhatIs2plus2 {...props} />);
 
     const combobox = screen.getByRole("combobox");
     await userEvent.selectOptions(combobox, "Not 4");
+    expect(mockFunc).toHaveBeenCalled();
+  });
 
-    // expect(combobox).toHaveValue("Not 4");
-    expect((screen.getByText("4") as HTMLOptionElement).selected).toBeFalsy();
+  it(`Given rendered component 
+  When invalid value is typed,
+  Then errorMessage should be present`, async () => {
+    const props = {
+      whatIs2plus2: "4" as Answer,
+      setWhatIs2plus2: jest.fn(),
+    };
+
+    render(<WhatIs2plus2 {...props} />);
+    const combobox = screen.getByRole("combobox");
+    await userEvent.selectOptions(combobox, "Not 4");
+    expect(screen.getByText(`This is not corect value!`)).toBeInTheDocument();
+  });
+
+  it(`Given rendered component 
+  When the valid value is typed,
+  Then errorMessage shouldn't be present`, async () => {
+    const props = {
+      whatIs2plus2: "4" as Answer,
+      setWhatIs2plus2: jest.fn(),
+    };
+
+    render(<WhatIs2plus2 {...props} />);
+    const combobox = screen.getByRole("combobox");
+    await userEvent.selectOptions(combobox, "4");
     expect(
-      (screen.getByText("Not 4") as HTMLOptionElement).selected
-    ).toBeTruthy();
+      screen.queryByText(`This is not corect value!`)
+    ).not.toBeInTheDocument();
   });
 });
--- src/components/WhatIs2plus2.tsx
+++ src/components/WhatIs2plus2.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
 
 export const ANSWERS = ["4", "Not 4"] as const;
 export type Answer = typeof ANSWERS[number];
@@ -17,9 +18,23 @@ const WhatIs2plus2: React.FC<WhatIs2plus2Props> = ({
   setWhatIs2plus2,
 }) => {
   const label = "What is 2 + 2?";
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
 
-  const onChangeWhatIs2plus2 = (value: string) => {
-    if (isOfAnswer(value)) setWhatIs2plus2(value);
+  const validate: (value: string) => string | undefined = value => {
+    const errorMessage = `This is not corect value!`;
+    if (value !== "4") return errorMessage;
+
+    return undefined;
+  };
+
+  const onChangeHandle = (value: string) => {
+    if (!isOfAnswer(value)) return;
+
+    const errMessage = validate(value);
+    setErrorMessage(errMessage);
+    setWhatIs2plus2(value);
   };
 
   return (
@@ -28,14 +43,17 @@ const WhatIs2plus2: React.FC<WhatIs2plus2Props> = ({
       <select
         name="whatIs2plus2"
         id="whatIs2plus2"
-        onChange={e => onChangeWhatIs2plus2(e.target.value)}
+        onChange={e => {
+          onChangeHandle(e.target.value);
+        }}
       >
         {ANSWERS.map(answ => (
-          <option value={whatIs2plus2} key={answ}>
+          <option value={answ} key={answ}>
             {answ}
           </option>
         ))}
       </select>
+      <ErrorMessage errorMessage={errorMessage} />
     </div>
   );
 };
