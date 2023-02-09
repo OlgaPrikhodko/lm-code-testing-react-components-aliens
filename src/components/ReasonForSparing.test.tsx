5c68a104af021472211b56f6567ff7a6e026223c
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-02-09 06:05:17 +0200

    add ReasonForSparing validation and tests

--- src/components/ReasonForSparing.test.tsx
+++ src/components/ReasonForSparing.test.tsx
@@ -26,12 +26,43 @@ describe("ReasonForSparing component", () => {
     const mockOnChange = jest.fn();
 
     const props = {
-      reasonForSparing: "We are really kind",
+      reasonForSparing: "",
       onChangeReasonOfSparing: mockOnChange,
     };
     render(<ReasonForSparing {...props} />);
     const textbox = screen.getByRole("textbox");
-    await userEvent.type(textbox, "We are really kind");
+    await userEvent.type(textbox, "We are really kind:)");
     expect(mockOnChange).toHaveBeenCalled();
+  });
+
+  it(`Given rendered component 
+      When the 'wrong' value is typed,
+      Then errorMessage should be present`, async () => {
+    const props = {
+      reasonForSparing: "",
+      onChangeReasonOfSparing: jest.fn(),
+    };
+    render(<ReasonForSparing {...props} />);
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "1");
+    expect(
+      screen.getByText(`Must be between 17 and 153 characters`)
+    ).toBeInTheDocument();
+  });
+
+  it(`Given rendered component 
+      When the valid value is typed,
+      Then errorMessage shouldn't be present`, async () => {
+    const props = {
+      reasonForSparing: "We are really kind:)".repeat(3),
+      onChangeReasonOfSparing: jest.fn(),
+    };
+    render(<ReasonForSparing {...props} />);
+
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "1");
+    expect(
+      screen.queryByText(`Must be between 17 and 153 characters`)
+    ).not.toBeInTheDocument();
   });
 });
--- src/components/ReasonForSparing.tsx
+++ src/components/ReasonForSparing.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+
 interface ReasonForSparingProps {
   reasonForSparing: string;
   onChangeReasonOfSparing(e: React.ChangeEvent<HTMLTextAreaElement>): void;
@@ -8,14 +11,29 @@ const ReasonForSparing: React.FC<ReasonForSparingProps> = ({
   onChangeReasonOfSparing,
 }) => {
   const label = "Reason for sparing";
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const validate: (value: string) => string | undefined = value => {
+    const errorMessage = `Must be between 17 and 153 characters`;
+    if (value.length < 17 || value.length > 153) return errorMessage;
+
+    return undefined;
+  };
   return (
     <div>
       <label htmlFor="reasonForSparing">{label}</label>
       <textarea
         name="reasonForSparing"
         value={reasonForSparing}
-        onChange={onChangeReasonOfSparing}
+        onChange={e => {
+          const errMessage = validate(e.target.value);
+          setErrorMessage(errMessage);
+          onChangeReasonOfSparing(e);
+        }}
       ></textarea>
+      <ErrorMessage errorMessage={errorMessage} />
     </div>
   );
 };
