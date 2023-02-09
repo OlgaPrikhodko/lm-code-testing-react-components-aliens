3a3f165b677f7efd4d9ad3a9cda0fcd3fbf870e0
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-02-09 04:58:43 +0200

    add NumberOfBeings validation and tests

--- src/components/NumberOfBeings.test.tsx
+++ src/components/NumberOfBeings.test.tsx
@@ -34,4 +34,33 @@ describe("NumberOfBeings", () => {
     await userEvent.type(input, "22");
     expect(mockChange).toHaveBeenCalled();
   });
+
+  it(`Given rendered component 
+  When the 'wrong' value is typed,
+  Then errorMessage should be present`, async () => {
+    const props = {
+      numberOfBeings: "",
+      onChangeNumberOfBeings: jest.fn(),
+    };
+    render(<NumberOfBeings {...props} />);
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "123");
+
+    expect(
+      screen.getByText(`Numbers ONLY. Must be at least 1,000,000,000`)
+    ).toBeInTheDocument();
+  });
+
+  it(`Given rendered component 
+  When the valid value is typed,
+  Then errorMessage shouldn't be present`, () => {
+    const props = {
+      numberOfBeings: "2000000000",
+      onChangeNumberOfBeings: jest.fn(),
+    };
+    render(<NumberOfBeings {...props} />);
+    expect(
+      screen.queryByText(`Numbers ONLY. Must be at least 1,000,000,000`)
+    ).not.toBeInTheDocument();
+  });
 });
--- src/components/NumberOfBeings.tsx
+++ src/components/NumberOfBeings.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ErrorMessage from "./ErrorMessage";
+
 interface NumberOfBeingsProps {
   numberOfBeings: string;
   onChangeNumberOfBeings(e: React.ChangeEvent<HTMLInputElement>): void;
@@ -8,15 +11,31 @@ const NumberOfBeings: React.FC<NumberOfBeingsProps> = ({
   onChangeNumberOfBeings,
 }) => {
   const label = "Number Of Beings";
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+
+  const validate: (value: string) => string | undefined = value => {
+    const errorMessage = `Numbers ONLY. Must be at least 1,000,000,000`;
+    if (value.length < 10 || !/^[0-9 ]+$/.test(value)) return errorMessage;
+
+    return undefined;
+  };
 
   return (
     <div>
       <label htmlFor="numberOfBeings">{label}</label>
       <input
+        id="numberOfBeings"
         type="text"
         value={numberOfBeings}
-        onChange={onChangeNumberOfBeings}
+        onChange={e => {
+          const errMessage = validate(e.target.value);
+          setErrorMessage(errMessage);
+          onChangeNumberOfBeings(e);
+        }}
       />
+      <ErrorMessage errorMessage={errorMessage} />
     </div>
   );
 };
